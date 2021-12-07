9b3cec05192db5905f199d11def11a80845256b0
Riley Bergamasco <rlbergamasco@gmail.com>
2021-12-07 17:37:44 -0500

    Redirect to share page after completed purchase

--- src/components/CheckoutButtons.jsx
+++ src/components/CheckoutButtons.jsx
@@ -1,51 +1,36 @@
 import React from "react";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
 export const CheckoutButtons = ({ amount }) => {
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: amount,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function (orderData) {
+      window.location.href = "/connect";
+    });
+  };
+
   return (
-    <PayPalScriptProvider
-      options={{ "client-id": process.env.REACT_APP_CLIENT_ID, "disable-funding": "paylater", "enable-funding": "venmo" }}
-    >
-      <PayPalButtons
-        style={{ layout: "vertical" }}
-        disabled={false}
-        forceReRender={[amount]}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  currency_code: "USD",
-                  value: amount,
-                },
-              },
-            ],
-          });
-        }}
-        onApprove={function (data, actions) {
-          return actions.order.capture().then(function (orderData) {
-            // Your code here after capture the order
-            console.log(
-              "Capture result",
-              orderData,
-              JSON.stringify(orderData, null, 2)
-            );
-            var transaction = orderData.purchase_units[0].payments.captures[0];
-            alert(
-              "Transaction " +
-              transaction.status +
-              ": " +
-              transaction.id +
-              "\n\nSee console for all available details"
-            );
-            actions.redirect('/connect');
-          });
-        }}
-      />
-    </PayPalScriptProvider>
+    <PayPalButton
+      disabled={true}
+      createOrder={(data, actions) => createOrder(data, actions)}
+      onApprove={(data, actions) => onApprove(data, actions)}
+    />
   );
-};
+}
 
 CheckoutButtons.propTypes = {
   amount: PropTypes.string.isRequired,
